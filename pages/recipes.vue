61eaf8239a62b6b2ba1264df1fd1fb15c682bae6
Felipe <felipe16.12.1998@gmail.com>
2024-12-22 21:48:50 -0300

    New functions and components

--- pages/recipes.vue
+++ pages/recipes.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="w-full flex flex-col items-center gap-4 relative">
+  <div class="w-full flex flex-col items-center gap-4 relative bg-primary-100">
     <header-menu />
+    <main class="container flex flex-col gap-4">
+      <div class="w-full">
+        <RecipeSearchRecipes :tags="tags" @search="searchByTags" />
+      </div>
+      <RecipeCards :recipes="recipes"  />
+      <!-- <pre>{{ recipes }}</pre> -->
+    </main>
   </div>
 </template>
+
+<script setup lang="ts">
+const recipes = ref([])
+const tags = await useFetchTags()
+
+onMounted(async () => {
+  const result = await useFetchRecipes()
+  recipes.value = result.recipes
+})
+
+const searchByTags = (tags: string[]) => {
+  console.log(tags)
+}
+
+
+
+
+</script>
