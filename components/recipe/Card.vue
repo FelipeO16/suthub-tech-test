9850e497ba9595d126cb6abe41e491745f3e1075
Felipe <felipe16.12.1998@gmail.com>
2024-12-22 21:47:15 -0300

    Add card components

---
+++ components/recipe/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="card max-w-sm bg-white border border-primary-200 rounded-lg shadow"
+  >
+    <img class="rounded-t-lg" :src="recipe.image" :alt="recipe.name" />
+    <div class="p-5">
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-primary-900 "
+      >
+        {{ recipe.name }}
+      </h5>
+      <div class="tags">
+        <RecipeTag v-for="tag in recipe.tags" :key="tag" class="mr-2">{{
+          tag
+        }}</RecipeTag>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { ShortRecipe } from "@/shared/types/recipes";
+
+const props = defineProps<{
+  recipe: ShortRecipe;
+}>();
+</script>
+
+<style scoped>
+.tags {
+  @apply mt-2 flex flex-wrap gap-2;
+}
+
+.card {
+  @apply bg-white shadow-md rounded-md hover:scale-105 transition-transform duration-300 ease-in-out;
+}
+</style>
---
+++ components/recipe/Cards.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="cards">
+    <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+    <!-- <div class="card col-span-1" v-for="recipe in recipes" :key="recipe.id">
+      <img :src="recipe.image" alt="Placeholder" class="w-full h-48 object-cover" />
+      <div class="p-4">
+        <h2 class="text-xl font-bold">{{ recipe.name }}</h2>
+        <div class="tags">
+          <RecipeTag v-for="tag in recipe.tags" :key="tag" class="mr-2">{{ tag }}</RecipeTag>
+        </div>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import type { ShortRecipe } from '@/shared/types/recipes'
+
+const props = defineProps<{
+  recipes: Array<ShortRecipe>
+}>()
+</script>
+
+<style scoped>
+.cards {
+  @apply grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 p-4;
+}
+
+
+
+
+</style>
