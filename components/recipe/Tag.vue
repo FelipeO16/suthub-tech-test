81fe5626d314123b71b8b07a8a10c1aa371bd126
Felipe <felipe16.12.1998@gmail.com>
2024-12-22 21:48:01 -0300

    Add Tag component

---
+++ components/recipe/Tag.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="px-2 py-1 bg-primary-100 text-primary-500 rounded-full text-xs uppercase cursor-pointer" @click="$emit('tag-selected')">
+      <slot />
+    </div>
+  </div>
+</template>
