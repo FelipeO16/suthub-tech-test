2c43bbe915033f42d4aabb121823478d8569f386
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-12-24 10:02:22 -0300

    Chage to sort by asc order

--- composables/getRecipesByTags.ts
+++ composables/getRecipesByTags.ts
@@ -1,13 +1,13 @@
 export default async function (tags: string[]) {
   if (tags.length) {
     const response = await $fetch(
-      `https://dummyjson.com/recipes/tag/${tags[0]}?&select=name,image,tags`
+      `https://dummyjson.com/recipes/tag/${tags[0]}?&select=name,image,tags&sortBy=name&order=asc`
     );
     const recipesByTag = response.recipes.filter((recipe) =>
       tags.every((tag) => recipe.tags.includes(tag))
     );
     return recipesByTag;
   }
-    const { data } = await useFetch("https://dummyjson.com/recipes?limit=10&skip=10&select=name,image,tags");
+    const { data } = await useFetch("https://dummyjson.com/recipes?limit=10&select=name,image,tags&sortBy=name&order=asc");
     return data.value.recipes; 
 }
--- composables/useFetchRecipes.ts
+++ composables/useFetchRecipes.ts
@@ -1,4 +1,4 @@
 export default async function() {
-  const response = await fetch('https://dummyjson.com/recipes?limit=10&skip=10&select=name,image,tags')
+  const response = await fetch('https://dummyjson.com/recipes?limit=10&select=name,image,tags&sortBy=name&order=asc')
   return response.json()
 }
