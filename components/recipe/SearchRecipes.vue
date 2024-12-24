b2f955b8e6ccf3ac2e8ecc0456541ad3a723f1a3
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-12-24 07:29:39 -0300

    Add function to search by multiple tags

--- components/recipe/SearchRecipes.vue
+++ components/recipe/SearchRecipes.vue
@@ -7,40 +7,43 @@
     >
       Busca por tags
     </div>
+    <div class="flex w-full flex-wrap gap-1">
+      <RecipeTag
+        v-for="tag in selectedTags"
+        :key="tag"
+        @remove="selectedTags.splice(selectedTags.indexOf(tag), 1)"
+        class=""
+        close-button
+        >{{ tag }}</RecipeTag>
+    </div>
     <div
       class="flex flex-col md:flex-row gap-16 md:gap-4 w-full min-h-40 md:min-h-16"
     >
       <div class="flex flex-col">
-        <h3 class="text-sm text-primary-400">Tag 1</h3>
+        <div class="text-sm text-primary-400 ml-1">Selecione uma tag</div>
         <div class="flex relative md:min-w-40 lg:min-w-72 w-full z-50">
           <Select
             :options="tags"
-            :selected-option="tags[0]"
-            v-model="selectedTags[0]"
-          />
-        </div>
-      </div>
-
-      <div class="flex flex-col">
-        <h3 class="text-sm text-primary-400">Tag 2</h3>
-        <div class="flex relative md:min-w-40 lg:min-w-72 w-full">
-          <Select
-            :options="tags"
-            :selected-option="tags[1]"
-            v-model="selectedTags[1]"
+            :selected-option="selectedTag"
+            v-model="selectedTag"
+            @update:modelValue="selectedTags.push(selectedTag)"
           />
         </div>
       </div>
       <div class="flex flex-col gap-2 md:flex-row">
         <button
-          class="bg-primary-500 text-white flex justify-center items-center rounded-md w-full md:w-36 h-10 self-end font-medium"
-          @click="$emit('search', selectedTags)"
+          :class="selectedTags.length === 0 ? 'bg-primary-300' : 'bg-primary-500'"
+          
+          class=" text-white flex justify-center items-center rounded-md w-full md:w-36 h-10 self-end font-medium"
+          @click="selectedTags.length > 0 ? $emit('search', selectedTags) : null"
         >
           Buscar
         </button>
         <button
+          
           class="text-primary-500 border border-primary-500 flex justify-center items-center rounded-md w-full md:w-36 h-10 self-end font-medium"
           @click="$emit('clearFilters'), (selectedTags = [])"
+
         >
           Limpar filtros
         </button>
@@ -56,6 +59,7 @@ const props = defineProps<{
 }>();
 
 const selectedTags = ref<Array<Tag>>([]);
+const selectedTag = ref("")
 </script>
 
 <style></style>
--- composables/getRecipesByTags.ts
+++ composables/getRecipesByTags.ts
@@ -1,22 +1,14 @@
 export default async function (tags: string[]) {
   const recipes = ref([]);
-  if (tags.length > 1) {
+  if (tags.length) {
     const {data} = await useFetch(
       `https://dummyjson.com/recipes/tag/${tags[0]}?&select=name,image,tags`
     );
     const recipesByTag = data.value.recipes.filter((recipe) =>
-      recipe.tags.includes(tags[1])
+      tags.every((tag) => recipe.tags.includes(tag))
     );
-    recipes.value = recipesByTag;
-  } else if (tags.length == 1) {
-    const {data} = await useFetch(
-      `https://dummyjson.com/recipes/tag/${tags[0]}?&select=name,image,tags`
-    );
-    recipes.value = data.value.recipes;
-  } else {
-    const { data } = await useFetch("https://dummyjson.com/recipes");
-    recipes.value = data.value.recipes; 
+    return recipesByTag;
   }
-
-  return recipes.value;
-};
+    const { data } = await useFetch("https://dummyjson.com/recipes?limit=10&skip=10&select=name,image,tags");
+    return data.value.recipes; 
+}
