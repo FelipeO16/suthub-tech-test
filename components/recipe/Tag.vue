20a7ec38e2567060748f248558d6b983ba0aa667
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-12-24 07:28:40 -0300

    Add close function to tag

--- components/recipe/Tag.vue
+++ components/recipe/Tag.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <div class="px-2 py-1 bg-primary-100 text-primary-500 rounded-full text-xs uppercase cursor-pointer" @click="$emit('tagSelected')">
+    <div class="px-2 py-1 bg-primary-100 text-primary-500 rounded-full text-xs uppercase cursor-pointer flex justify-center items-center" @click="$emit('tagSelected')">
       <slot />
+      <Icon v-if="closeButton" name="material-symbols:close-rounded" class="w-3 h-3 ml-1 cursor-pointer" @click="$emit('remove')" />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  closeButton: boolean;
+}>();
+</script>
